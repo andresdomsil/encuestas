4533c843dabff4d7ee1a27b75af737ae62907459
Andrés Domínguez <andres_domin_silva@outlook.com>
2017-08-01 17:42:43 -0500

    PHPmailer

--- controllersAjax/ajaxCorreos.php
+++ controllersAjax/ajaxCorreos.php
@@ -6,20 +6,19 @@ require_once '../controllers/controllerEncuestas.php';
 
 $obj = new controllerEncuestado();
 $obj2 = new controllerEncuestas();
+mail('andres_domin_silva@outlook.com','pruebas','algunas<br>pruebas de mail <br> Bien HD');
 $resp=$obj->visualizarEncuestadoControllerCorreo();
 foreach ($resp as $key) {
     $a=$obj2->verificarEncuestaContestadaController($key['idEncuestado']);
-    if($a!=null){
-        $mail = "Buenas tardes ".$key['nombre'].". Mediante este correo te pide tu apoyo el departamento de E2M del ITSLP.\r\n\r\n\r\n";
-        $mail .= "Te pedimos que entres a nuestra pagina web para realizar una encuesta que creamos para ti.\r\n\r\n";
+    if($a==null){
+        $mail = $key['nombre']."\nBuenos tardes.\n\nPor medio del presente y a nombre del departamento de Eléctrica, Electrónica y Mecatrónica les solicitamos de la manera más atenta su participación en una encuesta.\nPedimos su apoyo para  contestar una encuesta de seguimiento con el fin de revisar y actualizar el Plan de Estudios correspondiente a su carrera. Esto con la finalidad de complementar la información de acreditación por el Organismo CACEI que se realizará en el ITSLP, de antemano agradecemos su atención a la presente. La información recopilada será de gran interés para el Diseño y enriquecimiento  del Nuevo Módulo de Especialidad de las carreras de este departamento.\nAgradeciendo su atención a la presente y esperando contar con su apoyo quedamos a sus órdenes.\n\nAtentamente:\nM.C. Oscar Muñoz Cruz Jefe del Departamento Eléctrica, Electrónica y Mecatrónica\nM.I. Elizabeth Rivera BravoDocente del Departamento Eléctrica, Electrónica y Mecatrónica\nM.P.S. Patricia Méndez Ortiz Docente del Departamento Eléctrica, Electrónica y Mecatrónica";
         $mail .= "Accede a:\r\n\r\n http://encuestas.e2m.com.mx \r\n\r\n\r\n";
         $mail .= "Para entrar a la encuesta solo pon tu correo electronico: \r\n ".$key['correo']." \r\n\r\n";
-        $mail .= "Algun otro texto............. ";
 
         //Titulo
         $titulo = "Departamento E2M del TECNM-ITSLP te pide tu ayuda";
-        $bool = mail($key['correo'],$titulo,$mail);
-        echo $key['correo'];
+        //mail($key['correo'],$titulo,$mail);
+        
     }
 }
 $obj=null;
--- models/conexion.php
+++ models/conexion.php
@@ -4,8 +4,8 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=emcommx_sistema","emcommx_admin","@@@@Hola",array('charset'=>'utf8'));
-		//$link = new PDO("mysql:host=localhost;dbname=encuestas","root","",array('charset'=>'utf8'));
+		//$link = new PDO("mysql:host=localhost;dbname=emcommx_sistema","emcommx_admin","@@@@Hola",array('charset'=>'utf8'));
+		$link = new PDO("mysql:host=localhost;dbname=encuestas","root","",array('charset'=>'utf8'));
 		$link->query("SET CHARACTER SET utf8");
 		return $link;
 
